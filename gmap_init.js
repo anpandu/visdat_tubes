5fef649b6866aac9c8e2751117ffcc9a5aff7bc0
anpandu <anpandumail@gmail.com>
2014-05-12 18:07:07 +0700

    control panel di map

--- gmap_init.js
+++ gmap_init.js
@@ -11,7 +11,33 @@ function initialize() {
 		scaleControl: false,
 		scrollwheel: false,
 	};
-	var gmap = new google.maps.Map(document.getElementById("gmapcanvas"), gmapOptions);
-}			
+	var gmap = new google.maps.Map(document.getElementById("gmapcanvas"), gmapOptions);	
+
+	var regs = Object.keys(MASTER_DATA);
+	for (var i = 0; i < 2; i++) {
+		var marker = new google.maps.Marker({
+		    position: new google.maps.LatLng(-25.363882+i*10,131.044922+i*10),
+		    title: regs[i],
+    		animation: google.maps.Animation.DROP,
+		});
+		marker.setMap(gmap);
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+			return function() {
+				drawChart(marker.title.toLowerCase());
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+				setTimeout(function(){marker.setAnimation(null)}, 3000);
+			}
+		})(marker, i));
+	};
+}		
+
+function toogleBounce (marker) {
+	if (marker.getAnimation() != null) {
+		marker.setAnimation(null);
+	} else {
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+	}
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
 		
