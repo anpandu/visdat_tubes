45c3046cb9b47a63378923f5ab1d213a58703ba0
anpandu <anpandumail@gmail.com>
2014-05-12 17:06:20 +0700

    gabungin sama chart

--- gchart_init.js
+++ gchart_init.js
@@ -1,23 +1,28 @@
 
-			google.load("visualization", "1", {packages:["corechart"]});
-			var options = {
-			  title: 'School Enrollment',
-			  hAxis: {title: 'tahun', titleTextStyle: {color: 'black'}}
-			};
-			var options2 = {
-			  title: 'Infant Death',
-			  hAxis: {title: 'tahun', titleTextStyle: {color: 'black'}}
-			};
-			google.setOnLoadCallback(begin);
-			function begin () {
-				drawChart('indonesia');
-			}
-			function drawChart(_reg) {
-				var reg = (_reg!="") ? _reg : "indonesia";
-				var gdata = google.visualization.arrayToDataTable(MASTER_DATA[reg]["se"]);
-				var gdata2 = google.visualization.arrayToDataTable(MASTER_DATA[reg]["id"]);
-				var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-				var chart2 = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
-				chart.draw(gdata, options);
-				chart2.draw(gdata2, options2);
-			}
+google.load("visualization", "1", {packages:["corechart"]});
+var options = {
+	title: 'School Enrollment',
+	hAxis: {title: 'tahun', titleTextStyle: {color: 'black'}},
+	colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+	backgroundColor: '#B3D1FF',
+};
+var options2 = {
+	title: 'Infant Death',
+	hAxis: {title: 'tahun', titleTextStyle: {color: 'black'}},
+	colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+	backgroundColor: '#B3D1FF',
+};
+
+google.setOnLoadCallback(begin);
+function begin () {
+	drawChart('indonesia');
+}
+function drawChart(_reg) {
+	var reg = (_reg!="") ? _reg : "indonesia";
+	var gdata = google.visualization.arrayToDataTable(MASTER_DATA[reg]["se"]);
+	var gdata2 = google.visualization.arrayToDataTable(MASTER_DATA[reg]["id"]);
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	var chart2 = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
+	chart.draw(gdata, options);
+	chart2.draw(gdata2, options2);
+}
